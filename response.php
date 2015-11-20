b74025e92832bd6a701dac192126a3c5f19dfef5
Adjoa Maison <adjoa.maison@gmail.com>
2015-11-20 23:41:40 +0000

    added comments to the course and outline classes as well as the ajax response page

--- response.php
+++ response.php
@@ -1,4 +1,9 @@
-<?php    
+<?php   
+/**
+ * @author: Agatha Maison
+ * date: 20th November 2015
+ * description: A database class for all functions relating to the course outlines
+ */ 
     if(!isset($_REQUEST['cmd'])){
         echo '{"result":0,message:"unknown command"}';
         exit();
@@ -14,20 +19,24 @@
             echo '{"result":0,"message":"unknown command"}';
         break;
     }
-    
-        function add(){
-            $cid = $_REQUEST["cid"];
-            $cname = $_REQUEST["cname"];
-            $desc = $_REQUEST["cdesc"];
 
-            include("course.php");
-            $obj=new course();
-            
-            if($obj->addCourse($cid, $cname,$desc)) {               
-                echo '{"result":1}';        
-            }
-            else {
-                echo '{"result":0}';        
-            }
+/**
+ * description: A function that takes parameters from a given url to add a new
+ * course to the database. It echos a json with result being 1 when successful 
+ * or 0 if query failed
+ */
+    function add(){
+        $cid = $_REQUEST["cid"];
+        $cname = $_REQUEST["cname"];
+        $desc = $_REQUEST["cdesc"];
+
+        include("course.php");
+        $obj=new course();
+          
+        if($obj->addCourse($cid, $cname,$desc)) {               
+            echo '{"result":1}';        
+        }else {
+            echo '{"result":0}';        
         }
+    }
 ?>
