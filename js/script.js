ddddcf68a7bf10886073d11992da9864668a8753
Adjoa Maison <adjoa.maison@gmail.com>
2015-11-21 01:33:46 +0000

    an onlick function to view courses

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function sendRequest(u){
     return result;  //return object
 }
 
+//add course to database
 function addCourse(){
 	var msg = "";
 	if(($("#cid").val().length==0) || ($("#cname").val().length==0) || ($("#cdesc").val().length==0) ){
@@ -35,6 +36,28 @@ function addCourse(){
 	    	msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
 	    	msg += "<span aria-hidden='true'>&times;</span></button>";
 	        msg += "Addition of course was unsuccessful.</div>";
+		}
+	}
+	$("#alert").html(msg);
+}
+
+//view courses
+function viewCourse(){
+	var msg="";
+	var strUrl = "response.php?cmd=1&cid=2";
+		var objResult=sendRequest(strUrl);
+		if(objResult.result==1){
+			msg = "";			
+	    	msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    	msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    	msg += "<span aria-hidden='true'>&times;</span></button>";
+	        msg += "List of All Courses.</div>";
+		}else{
+			msg = "";
+	    	msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    	msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    	msg += "<span aria-hidden='true'>&times;</span></button>";
+	        msg += "Unsuccessful.</div>";
 		}
 	}
 	$("#alert").html(msg);
