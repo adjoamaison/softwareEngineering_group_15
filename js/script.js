97fe2c82653eacd4ebad0595c1f0b205ff76c958
Adjoa Maison <adjoa.maison@gmail.com>
2015-12-15 12:34:32 +0000

    complete addition of add course outline in js

--- js/script.js
+++ js/script.js
@@ -17,11 +17,11 @@ function addCourse(){
 		var c=$("#cid").val();
 		var n=$("#cname").val();
 		var d=$("#cdesc").val();
-		
+
 		var strUrl = "response.php?cmd=1&cid="+c+"&cname="+n+"&cdesc="+d;
 		var objResult=sendRequest(strUrl);
 		if(objResult.result==1){
-			msg = "";			
+			msg = "";
 	    	msg += "<div class='alert alert-success alert-dismissible' role='alert'>";
 	    	msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
 	    	msg += "<span aria-hidden='true'>&times;</span></button>";
@@ -56,7 +56,7 @@ function viewCourse(){
         list += "<thead><tr><th>Courses ID</th>";
         list += "<th>Course Name</th><th>Description</th>";
         list += "<th></th></tr></thead><tbody>";
-	    for ( var i = 0; i<objResult.courses.length; i++) {  
+	    for ( var i = 0; i<objResult.courses.length; i++) {
 			list += "<tr><td>"+objResult.courses[i].course_id+"</td>";
         	list += "<td>"+objResult.courses[i].course_name+"</td>";
         	list += "<td>"+objResult.courses[i].course_description+"</td>";
@@ -64,7 +64,7 @@ function viewCourse(){
 	    }
 	    list += "</tbody></table>";
 	    $("#content").html(list);
-			msg = "";			
+			msg = "";
 	    	msg += "<div class='alert alert-success alert-dismissible' role='alert'>";
 	    	msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
 	    	msg += "<span aria-hidden='true'>&times;</span></button>";
@@ -76,8 +76,45 @@ function viewCourse(){
 	    	msg += "<span aria-hidden='true'>&times;</span></button>";
 	        msg += "Unsuccessful.</div>";
 		}
-	
+
 	$("#alert").html(msg);
 }
- 
-  
+
+function addOutline(){
+  var id = makeid();
+  var cid=course.value;
+  var obj=objective.value;
+  var goal=goal.value;
+  var res=resource.value;
+  var schedule =[];
+  var topic=topic.value;
+  var week=week.value;
+  var read = read.value;
+
+  for (var i = 0; i < count; i++) {
+    testd = "schedule";
+    var countd = testd.concat(i);
+    schedule[i]= document.getElementById(countd).value;
+    alert("schedule..." +schedule[i]);
+    var strUrl = "response.php?cmd=4&id="+id+"&week="+schedule[i]+"&topic="+topic+"&read="+read;
+  		var objResult=sendRequest(strUrl);
+  }
+  var msg="";
+	var strUrl = "response.php?cmd=3&id="+id+"&cid="+cid+"&obj="+objective+"&goal="+goal+"&res="+resource;
+		var objResult=sendRequest(strUrl);
+		if(objResult.result==1){
+      msg = "";
+      msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+      msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+      msg += "<span aria-hidden='true'>&times;</span></button>";
+      msg += "Recipe succefully added.</div>";
+
+    }else{
+			msg = "";
+	    msg += "<div class='alert alert-warning alert-dismissible' role='alert'>";
+	    msg += "<button type='button' class='close' data-dismiss='alert' aria-label='Close'>";
+	    msg += "<span aria-hidden='true'>&times;</span></button>";
+	    msg += "Could not add recipe.</div>";
+		}
+    $("#alert").html(msg);
+}
